74c8b4b14446fa27e48eb5fbbc239b48684d97c6
jopz-123 <josephfrancizz009@gmail.com>
2021-06-01 12:02:07 +0530

    message

---
+++ resources/views/index.blade.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+</head>
+<body style="background-color: #0c0c0c;">
+  <nav class="navbar navbar-light" style="background-color: #ffffff;" style="color: rgb(252, 247, 247);">
+    <!-- Navbar content -->
+    <div class="container-fluid" style="background-color: #e21373;">
+      <a class="navbar-brand" href="#">We care.We cure <marquee><h1> <font style="font-family: 'Times New Roman', Times, serif ;color: rgb(253, 250, 250);"><b>Rajagiri Hospital</b></font></h1></marquee></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span style="background-color: #3c2a68;" class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <form class="d-flex">
+              <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit" style="font-size: 20px;color: rgb(12, 12, 12);">Search</button>
+            </form>
+            <a class="nav-link active" aria-current="page" href="/" style="font-size: 20px;color: rgb(247, 252, 247);">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://patientportal.rajagirihospital.com" style="font-size: 20px;color: rgb(247, 252, 247);">Online Appointment</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.rajagirihospital.com/Patient-Care" style="font-size: 20px;color: rgb(247, 252, 247);">Patient care</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="https://www.rajagirihospital.com/Find-A-Doctor" style="font-size: 20px;color: rgb(247, 252, 247);">Find A Doctor</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false"style="font-size: 20px;color: rgb(247, 252, 247);">
+              My Account
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item"  href="https://patientportal.rajagirihospital.com" style="font-size: 20px;color: rgb(8, 8, 8);"> Login</a></li>
+              <li><a class="dropdown-item"  href="https://patientportal.rajagirihospital.com" style="font-size: 20px;color: rgb(20, 20, 20);" >Register</a></li>
+              <li><a class="dropdown-item" href="#" style="font-size: 20px;color: rgb(8, 8, 8);"></a></li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="/gallery" tabindex="-1" aria-disabled="true" style="font-size: 20px;color: rgb(247, 252, 247);">Photo Gallery</a>
+          </li>
+        </ul>
+        <ul class="list-unstyled" style="font-size: 20px;color: rgb(19, 18, 18);">
+          <li>Phone: 91 - 484 - 2660601</li>
+          <li>Fax: 91 - 484 - 2426578</li>
+      </ul>
+        
+      </div>
+    </div>
+</nav>
+  <nav class="navbar navbar-light bg-light" >
+    <div class="container-fluid">
+      <span class="navbar-text">
+        <div class="container">
+            <div class="row">
+                <div class="col col-12 col-sm-5 col-lg-5 col-md-5 col-xl-12 col-xxl-12" >
+                    <img height="10px" src="https://www.rajagirihospital.com/images/logo.png" class="img-thumbnail" alt="..." >
+                </div>
+                
+            </div>
+        </div>
+      </span>
+    </div>
+  </nav>
+
+
+
+  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="https://www.rajagirihospital.com/Userfiles/Poster/4a34112418994ad8862e504168ae0691.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.rajagirihospital.com/Userfiles/Poster/3ec80babc898461987451051e9673eb7.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.rajagirihospital.com/Userfiles/Poster/4a34112418994ad8862e504168ae0691.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="https://www.rajagirihospital.com/Userfiles/Poster/4a34112418994ad8862e504168ae0691.jpg" class="d-block w-100" alt="...">
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+            
+
+
+
+
+
+
+
+
+
+
+
+            <div class="carousel-item">
+              
+    
+            <div class="container">
+              <div class="row">
+                <div class="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                  <div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+                    <div class="carousel-inner">
+                      <div class="slider carousel slide" data-ride="carousel">
+                        <!-- Wrapper for slides -->
+                        <div class="carousel-inner" role="listbox">
+                          <div class="item active" id="video">
+                            <video id="video" muted autoplay="autoplay" loop="loop" preload="auto">
+                              <source src="https://www.mbusa.com/content/dam/mb-nafta/us/homepage-redesign/HKWA_S_DMR.mp4"></source>
+                            </video>
+                          </div>
+                          <div class="item">
+                            <img src="assets/img/slide-2.png" alt="Slide 2">
+                          </div>
+                        </div>
+                        <div class="slider-layout"></div>
+                      </div>
+                      
+
+            
+            <div class="carousel-item">
+              <img src="http://jnanajyothividyasamsthe213cng.com/galleryimages/August2017/gallery_703_201708211100000.086913001503293400.jpg" class="d-block w-100" alt="...">
+            </div>
+          </div>
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade"  data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
+        </div>
+
+      </div>
+    </div>
+  </div>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
